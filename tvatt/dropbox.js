15d9ccb1bbb36b78e516737f420630569a509337
Johan Lundquist <jolundq@gmail.com>
2018-11-05 21:05:03 +0100

    wip

--- tvatt/dropbox.js
+++ tvatt/dropbox.js
@@ -1,3 +1,5 @@
+let days = ['', 'M', 'T', 'O', 'T', 'F', 'L', 'S'];
+
 new Dropbox.Dropbox({
     fetch: fetch,
     accessToken: 'l1NK-ZXx8_AAAAAAAAAAjxQLHJDkg_JoGi-Ki7TguhOnDwp0dEvH9bVPHdUrlAVP'
@@ -7,4 +9,16 @@ new Dropbox.Dropbox({
     .filesListFolder({path: ''})
     .then(function(entries) {
         console.log(entries.entries[0].name);
+        $("#weeks").html(entries.entries[0].name);
+
+        for (let day = 0; day <= 7; day++) {
+            $("#weekgrid").append($("<div class='grid-item'>" + days[day] + "</div>"))
+        }
+
+        for (let hour = 7; hour<=22; hour++) {
+            $("#weekgrid").append($("<div class='grid-item'>" + hour + "</div>"));
+            for (let day = 0; day <= 6; day++) {
+                $("#weekgrid").append($("<div class='grid-item'>" + "" + "</div>"))
+            }
+        }
     }, console.error);
