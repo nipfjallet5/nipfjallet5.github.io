c9a09058fc53de4883de19470ea89c9a44091554
Johan Lundquist <jolundq@gmail.com>
2018-11-09 09:52:19 +0100

    wip

--- tvatt/auth.js
+++ tvatt/auth.js
@@ -16,18 +16,138 @@ console.log('hejsan');
 //     });
 // }
 
+function hideKeyboard(element) {
+    element.attr('readonly', 'readonly'); // Force keyboard to hide on input field.
+    element.attr('disabled', 'true'); // Force keyboard to hide on textarea field.
+    setTimeout(function() {
+        element.blur();  //actually close the keyboard
+        // Remove readonly attribute after keyboard is hidden.
+        element.removeAttr('readonly');
+        element.removeAttr('disabled');
+    }, 100);
+}
+
+class WeekSelector extends HTMLElement {
+    constructor() {
+        super();
+        let template = document.createElement('template');
+        template.innerHTML = `
+            <!--<style>-->
+            <!--</style>-->
+            <div class="ui-grid-b">
+                <div class="ui-block-a"><input class="weekSelectButton" id="previous" type="button" value="förra"></div>
+                <div class="ui-block-b"><input class="weekSelectButton" id="current" type="button" value="denna"></div>
+                <div class="ui-block-c"><input class="weekSelectButton" id="next" type="button" value="nästa"></div>
+            </div>
+        `;
+        // this.attachShadow({mode: 'open'}).appendChild(template.content.cloneNode(true));
+        this.appendChild(template.content.cloneNode(true));
+        console.log('WeekSelector constructed');
+    }
+
+    connectedCallback() {
+        console.log('WeekSelector connected');
+        $('.weekSelectButton').button().button('refresh');
+        $('.weekSelectButton#previous').click(event => {console.log('previous week');});
+        $('.weekSelectButton#current').click(event => {console.log('current week');});
+        $('.weekSelectButton#next').click(event => {console.log('next week');});
+    }
+}
+
+customElements.define('week-selector', WeekSelector);
+
+
 let accessToken = "U2FsdGVkX189p19Ob4DSM/9t8eIFgKOYYEKDM4ekNsC4VsMFP3pxSm7jPgao6UTwe89bkrrd2zgL+d0sISLA6jW7nc+7HUpUHw8YRxMeqPAsLGHpenmbNddMIYwNlB5N";
 
-$('#password-1').on('keyup',function(event) {
+
+// $(document).ready(function(){
+//
+//     // let password = CryptoJS.SHA256('password').toString();
+//
+//     $.get('/assets/enc/data.json.enc', data => {
+//         try {
+//             let info = JSON.parse(CryptoJS.AES.decrypt(data, password).toString(CryptoJS.enc.Utf8));
+//
+//             hideKeyboard($(this));
+//
+//             let content = $('#content');
+//
+//             Object.entries(info.apartments).map(([apartmentNumber, name]) => {
+//                 console.log(apartmentNumber, name);
+//
+//                 let apartmentButton = $('<input type="button">')
+//                     .attr('value', apartmentNumber + '. ' + name)
+//                     .appendTo(content)
+//                     .button({
+//                         mini: true
+//                     }).button('refresh')
+//                     .click(event => {
+//                         console.log('selecting', apartmentNumber + '. ' + name);
+//                         localStorage.setItem('apartment', apartmentNumber);
+//                         content.html('');
+//                         // let ws = new WeekSelector();
+//                         content.append($(new WeekSelector()));
+//                         // content.page('refresh', true);
+//                     })
+//                     .parent().each((index,item) => {
+//                         $(item).addClass('apartment-selector');
+//                     });
+//             });
+//         }
+//         catch (e) {
+//         }
+//     }, 'text');
+//
+//
+// });
+
+$('#password').on('keyup',function(event) {
     if ($(this).val().length === 4) {
-        console.log('checking password');
+        console.log('checking password', this);
         let password = CryptoJS.SHA256($(this).val()).toString();
 
-        $.get('/assets/enc/data.json.enc', function(data) {
-            let info = JSON.parse(CryptoJS.AES.decrypt(data, password).toString(CryptoJS.enc.Utf8));
-            console.log(info);
-            $('#debug').html(JSON.stringify(info));
+        $.get('/assets/enc/data.json.enc', data => {
+            try {
+                let info = JSON.parse(CryptoJS.AES.decrypt(data, password).toString(CryptoJS.enc.Utf8));
+
+                hideKeyboard($(this));
+
+                let content = $('#content');
+
+                Object.entries(info.apartments).map(([apartmentNumber, name]) => {
+                    console.log(apartmentNumber, name);
+
+                    let apartmentButton = $('<input type="button">')
+                        .attr('value', apartmentNumber + '. ' + name)
+                        .appendTo(content)
+                        .button({
+                            mini: true
+                        }).button('refresh')
+                        .parent().each((index,item) => {
+                            $(item).addClass('apartment-selector');
+                        })
+                        .click(event => {
+                            console.log('selecting', apartmentNumber + '. ' + name);
+                            localStorage.setItem('apartment', apartmentNumber);
+                            content.html('');
+                            // let ws = new WeekSelector();
+                            content.append($(new WeekSelector()));
+                            // content.page('refresh', true);
+                        });
+                });
+            }
+            catch (e) {
+            }
         }, 'text');
+
+
+        // $.get('/assets/enc/data.json.enc', function(data) {
+        //     let info = JSON.parse(CryptoJS.AES.decrypt(data, password).toString(CryptoJS.enc.Utf8));
+        //     console.log(info);
+        //     $('#debug').html(JSON.stringify(info));
+        //
+        //
+        // }, 'text');
 
         // let dropbox = new Dropbox.Dropbox({
         //     fetch: fetch,
